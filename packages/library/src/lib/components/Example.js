e34ed61ef8d3c255a8929a93e3cf911a025c5b65
danydhondt <archemedia@mac.com>
2021-03-25 09:26:23 +0100

    Initial commit

---
+++ packages/library/src/lib/components/Example.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import udiliaLogo from './udilia-logo.svg';
+import './Example.css';
+import {Header, Button} from "semantic-ui-react";
+
+const Example = () => (
+  <div className="Example">
+    <Header>This button is in the library</Header>
+      <Button>Library button</Button>
+  </div>
+);
+
+export default Example;
